8924dc8ef6a2fc1f86fc6c4b10f28735bf59c642
PDC2B-Training.PDC2B <PDC2B-Training.PDC2B@P2B-6B6DNK2.dir.svc.accenture.com>
2019-07-29 11:42:28 +0530

    Changes on 29th July

--- src/test/java/skeleton/RunCukesTest.java
+++ src/test/java/skeleton/RunCukesTest.java
@@ -6,7 +6,7 @@ import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber;
 @RunWith(Cucumber.class)
 @CucumberOptions(plugin= {"json:target/cucumber.json"},
-tags="@datatable")
+tags="@data-outline")
 public class RunCukesTest{
    //changes on 29th July
 }
